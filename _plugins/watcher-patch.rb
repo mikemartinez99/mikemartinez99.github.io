0f0468cfb30fef7fe0435274cce0b6b6f0f5951f
mikemartinez99 <mike.j.martinez99@gmail.com>
2025-12-02 20:14:11 -0500

    Update watcher-patch.rb

--- _plugins/watcher-patch.rb
+++ _plugins/watcher-patch.rb
@@ -2,14 +2,14 @@
 
 require 'jekyll-watch'
 
-module jekyll
+module Jekyll
     module Watcher
         extend self
 
         alias method :original_listen_ignore_paths, :listen_ignore_paths
 
         def listen_ignore_paths(options)
-            original_listen_ignore_paths(options) + [%r!.*/.TMP!i]
+            original_listen_ignore_paths(options) + [%r!.*/\.TMP!i]
         end 
     end 
 end
