bbd4022684792825ee7d5269ee39334a9992ab3e
mikemartinez99 <mike.j.martinez99@gmail.com>
2025-12-02 20:15:45 -0500

    Update watcher-patch.rb

--- _plugins/watcher-patch.rb
+++ _plugins/watcher-patch.rb
@@ -1,15 +1,15 @@
-# frozen string literal: true
+# frozen_string_literal: true
 
 require 'jekyll-watch'
 
 module Jekyll
-    module Watcher
-        extend self
+  module Watcher
+    extend self
 
-        alias method :original_listen_ignore_paths, :listen_ignore_paths
+    alias_method :original_listen_ignore_paths, :listen_ignore_paths
 
-        def listen_ignore_paths(options)
-            original_listen_ignore_paths(options) + [%r!.*/\.TMP!i]
-        end 
-    end 
+    def listen_ignore_paths(options)
+      original_listen_ignore_paths(options) + [%r!.*/\.TMP!i]
+    end
+  end
 end
